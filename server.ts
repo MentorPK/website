ca33f3f4bd65b8338652375dc1c890802db42795
MentorPK <kowalski.pawel21@gmail.com>
2023-07-23 14:18:21 +0200

    FIXED handler for deployment

--- server.ts
+++ server.ts
@@ -4,4 +4,7 @@ import { createRequestHandler } from '@remix-run/netlify';
 
 installGlobals();
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export const handler = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
