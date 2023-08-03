a1c50242689cfcb1bbf4f3de902bd76e0bf58a34
MentorPK <kowalski.pawel21@gmail.com>
2023-08-03 21:57:08 +0200

    ADDED contact feedback

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -8,35 +8,47 @@ interface Checkbox {
 
 const Checkbox = ({ label, setChecked }: Checkbox) => {
   const [check, setCheck] = useState(false);
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.checked;
-    setCheck(value);
-    setChecked(value);
+  const handleChange = (e?: ChangeEvent<HTMLInputElement>) => {
+    if (e) {
+      const value = e.target.checked;
+      setCheck(value);
+      setChecked(value);
+    } else {
+      setCheck(!check);
+      setChecked(!check);
+    }
   };
 
   return (
-    <div className="flex items-center gap-2">
+    <div
+      className="flex items-center gap-4 cursor-pointer select-none my-2"
+      onClick={() => handleChange()}
+    >
       <input
         type="checkbox"
-        className="appearance-none w-[20px] h-[20px] border-2 border-slate-500 rounded checked:bg-transparent bg-secondary shrink-0 checked:border-primary relative peer hover:border-primary transition-all duration-200 ease-in-out"
+        className="cursor-pointer appearance-none w-[20px] h-[20px] border-2 border-slate-500 rounded checked:bg-transparent bg-secondary shrink-0 checked:border-primary relative peer hover:border-primary transition-all duration-200 ease-in-out"
+        checked={check}
         onChange={(e) => handleChange(e)}
       />
       <label
         className={`${
           !check ? 'opacity-70' : 'opacity-100'
-        } transition-all duration-200 ease-in-out`}
+        } transition-all duration-200 ease-in-out cursor-pointer`}
       >
         {label}
       </label>
       <svg
-        className="absolute w-[20px] h-[20px] hidden peer-checked:block pointer-events-none text-primary"
+        className={`absolute w-[20px] h-[20px] hidden peer-checked:block pointer-events-none text-primary 
+        ${
+          check ? 'opacity-100' : 'opacity-0'
+        } transition-all duration-200 ease-in-out`}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <polyline points="20 6 9 17 4 12"></polyline>
       </svg>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,49 @@ export default {
       boxShadow: {
         box: '0 2px 2.5em 0 rgba(1,1,2,.28)',
       },
+      keyframes: {
+        'spinner-grow': {
+          '0%': {
+            transform: 'scale(0)',
+          },
+          '50%': {
+            transform: 'none',
+            opacity: '1',
+          },
+        },
+        pop: {
+          '25%': {
+            transform: 'scale(0.7)',
+          },
+          '75%': {
+            transform: 'scale(1.3)',
+          },
+        },
+        shake: {
+          '0%': {
+            transform: 'scale3d(1, 1, 1)',
+          },
+          '10%, 20%': {
+            transform: 'scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg)',
+          },
+          '30%, 50%, 70%, 90%': {
+            transform: 'scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg)',
+          },
+          '40%, 60%, 80%': {
+            transform: 'scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg)',
+          },
+          '100%': {
+            transform: 'scale3d(1, 1, 1)',
+          },
+        },
+      },
+    },
+    //TODO: fix
+    animationDelay: {
+      'animate-delay-0': '0s',
+      'animate-delay-100': '100ms',
+      'animate-delay-200': '200ms',
+      'animate-delay-300': '300ms',
     },
     container: {
       center: true,
