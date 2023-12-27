8c82ee9bdea7b810fe9f9c577d90c2df082a6acb
MentorPK <kowalski.pawel21@gmail.com>
2023-12-27 18:27:06 +0100

    CHANGED hihlighter theme

--- app/components/learningByDoing/Highlighter.tsx
+++ app/components/learningByDoing/Highlighter.tsx
@@ -3,13 +3,13 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { useEffect, useState } from 'react';
 
 type Highlighter = {
-  children: React.ReactNode;
+  children: string;
 };
 
 const Highlighter = ({ children }: Highlighter) => {
   const [style, setStyle] = useState({});
   useEffect(() => {
-    import('react-syntax-highlighter/dist/esm/styles/prism/material-dark').then(
+    import('react-syntax-highlighter/dist/esm/styles/prism/a11y-dark').then(
       (mod) => setStyle(mod.default)
     );
   });
--- app/routes/learning-by-doing.tsx
+++ app/routes/learning-by-doing.tsx
@@ -3,13 +3,26 @@ import Footer from '~/components/Footer';
 import Header from '~/components/Header';
 import Highlighter from '~/components/learningByDoing/Highlighter';
 
+const code = `const LearningByDoing = () => {
+  return (
+    <div className="flex flex-col justify-between min-h-screen bg-secondary">
+      <Header />
+      <Container>
+        <div className="flex 2xl:flex-row flex-col gap-4 my-12 mx-2">
+          <Highlighter>
+        </div>
+      </Container>
+      <Footer />
+    </div>
+  );
+};`;
 const LearningByDoing = () => {
   return (
     <div className="flex flex-col justify-between min-h-screen bg-secondary">
       <Header />
       <Container>
         <div className="flex 2xl:flex-row flex-col gap-4 my-12 mx-2">
-          <Highlighter>{'const mat = dat'}</Highlighter>
+          <Highlighter>{code}</Highlighter>
         </div>
       </Container>
       <Footer />
